64211ed964ed29e52316b42ac007439c6c622da6
Shulhan Andrei <shulhan.andrei@gmail.com>
2020-02-24 13:27:07 +0300

    feat: finished task

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -1,7 +1,15 @@
 const MODERN_ACTIVITY= 15; 
 const HALF_LIFE_PERIOD= 5730;
 
-module.exports = function dateSample(/* sampleActivity */) {
-  throw 'Not implemented';
-  // remove line with error and write your code here
+module.exports = function dateSample(n) {
+  if( (typeof n) != 'string' || isNaN(n) ) {
+    return false
+  }
+  let rez = (Math.log(MODERN_ACTIVITY / parseFloat(n))) / 1.2094;
+  let a = Math.ceil(rez * 10000);
+  if (a < 0) {
+    return false
+  }
+  return a;
 };
+
--- src/what-season.js
+++ src/what-season.js
@@ -1,4 +1,20 @@
-module.exports = function getSeason(/* date */) {
-  throw 'Not implemented';
-  // remove line with error and write your code here
+module.exports = function getSeason(date = 'undefined') {
+
+  if (date == 'undefined') {
+    return 'Unable to determine the time of year!'
+  }
+  if (!date instanceof Date || date.hasOwnProperty('toString')) throw new Error('Error');
+
+  let month = date.getMonth();
+
+  if ((month <= 1) || month == 11) {
+    return 'winter'
+  } else if (month >= 2 && month <= 4) {
+    return 'spring'
+  } else if (month >= 5 && month <= 7) {
+    return 'summer'
+  } else {
+    return 'autumn'
+  }
+
 };
