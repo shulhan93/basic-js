9275e994084109bb74bac755e4ef912fb96546d4
Shulhan Andrei <shulhan.andrei@gmail.com>
2020-03-03 23:39:07 +0300

    feat: finished task

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,4 +1,34 @@
-module.exports = function transform(/* arr */) {
-    throw 'Not implemented';
-    // remove line with error and write your code here
-};
+ module.exports = function transform(arr) {
+    if (!Array.isArray(arr)) {
+        throw 'Error'
+    }
+    let rez = arr.reduce((sum, cur, i) => {
+
+        if (cur == '--double-next') {
+            if (arr[i + 1] == undefined) {
+                return sum
+            }
+            cur = arr[i + 1]
+        } else if (cur == '--double-prev') {
+            if (arr[i - 1] == undefined) {
+                return sum
+            }
+            cur = arr[i - 1]
+        } else if (cur == '--discard-prev') {
+            if (arr[i - 1] == undefined) {
+                return sum
+            }
+            sum.splice(sum.length - 1, 1)
+            return sum
+        } else if (cur == '--discard-next') {
+            if (arr[i + 1] == undefined) {
+                return sum
+            }
+            arr.splice(i, 1)
+            return sum
+        }
+        sum.push(cur)
+        return sum
+    }, [])
+    return rez
+}; 
