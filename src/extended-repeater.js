a7ae1548d70f7599a42ce902fb9704619d9a74b1
Shulhan Andrei <shulhan.andrei@gmail.com>
2020-03-19 21:20:42 +0300

    feat: finished task

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,5 +1,22 @@
-module.exports = function repeater(/* str, options */) {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+module.exports = function repeater(str, {
+    repeatTimes = 1,
+    separator = '+',
+    addition = '',
+    additionRepeatTimes = 1,
+    additionSeparator = '|'
+}) {
+    let res = ''
+    let substr = str;
+    for (let i = 0; i < repeatTimes; i++) {
+        for (let j = 0; j < additionRepeatTimes; j++) {
+            substr = j < additionRepeatTimes - 1 ?
+                substr + String(addition) + additionSeparator :
+                substr + String(addition)
+        }
+        res = i < repeatTimes - 1 ?
+            substr + separator + res :
+            res + substr
+        substr = str
+    }
+    return res
 };
-  
